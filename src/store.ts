f6fd4a96957c939550c40424625a1141fb7eed2f
Hayden Heroux <haydenheroux@gmail.com>
2022-06-28 22:44:00 -0400

    Update assignments with test data.

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,16 @@
 import { writable } from "svelte/store";
 
-export const assignments = writable([]);
+export const assignments = writable([
+	{
+		name: "Chemistry of Toys",
+		isSummative: false,
+		possible: null,
+		current: 80,
+	},
+	{
+		name: "Final Exam",
+		isSummative: true,
+		possible: 90,
+		current: null,
+	}
+]);
