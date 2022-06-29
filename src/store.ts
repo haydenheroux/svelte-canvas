abde29dfdb5494b64fd8f766d05a4853baa6f8e9
Hayden Heroux <haydenheroux@gmail.com>
2022-06-29 18:05:34 -0400

    fix: Update test data.

--- src/store.ts
+++ src/store.ts
@@ -6,11 +6,20 @@ export const assignments = writable([
 		isSummative: false,
 		possible: null,
 		current: 80,
+		max: 100
+	},
+	{
+		name: "Titrations",
+		isSummative: false,
+		possible: null,
+		current: 20,
+		max: 40
 	},
 	{
 		name: "Final Exam",
 		isSummative: true,
 		possible: 90,
 		current: null,
+		max: 100
 	}
 ]);
